664354370222909a7be6ae950f165f23914ea196
pranavkhapra <pranavkhapra23@gmail.com>
2021-04-05 15:59:42 +0530

    rendering components based on our data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import FollowersColumn from '../components/FollowersColumn';
+import Card from '../components/Card';
 import '../App.css';
 
 function Home() {
-  const [users, setUsers] = useState();
+  const [users, setUsers] = useState(null);
+  let descendingUsers;
   // first we need to populate our data
   // bascially so that we dont need to visit the page to add data
   const addData = async () => {
@@ -19,13 +21,34 @@ function Home() {
     addData();
     fetchData();
   }, []);
+
+  // bascially i want to sort the user in descending order of their id which can be sort by .sort()
+  if (users) {
+    descendingUsers = users.sort((a, b) => (a.id < b.id ? 1 : -1));
+  }
+
   return (
-    <div className="container">
-      Home
-      <FollowersColumn />
-      <div className="feed" />
-      <div className="suggested-box" />
-    </div>
+    // if descending user exist only we want to show this user
+    <>
+      {descendingUsers && (
+        <div className="container">
+          <FollowersColumn />
+          <div className="feed">
+            {descendingUsers.map((descendingUser, index) => (
+              <Card key={index} user={descendingUser} />
+            ))}
+          </div>
+          <div className="suggested-box">
+            <div className="section">
+              <div className="suggested">
+                <h2 className="bold">Suggested Accounts</h2>
+                <div className="break" />
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
